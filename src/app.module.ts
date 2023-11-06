70cacab9a4c16985a8b99fc12fb0ff3f3f817292
relliv <epsilonelittr@gmail.com>
2023-11-06 05:43:01 +0300

    Add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,24 @@ import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
 import { ConfigModule } from "@nestjs/config"
 import { UserModule } from "@modules/user/user.module"
+import { TypeOrmModule } from "@nestjs/typeorm"
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    UserModule
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DATABASE_HOST || "localhost",
+      port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+      username: process.env.DATABASE_USER || "root",
+      password: process.env.DATABASE_PASSWORD || "",
+      database: process.env.DATABASE_NAME || "nest_starter_typeorm_mysql",
+      entities: [__dirname + "/app/**/*.entity{.ts,.js}"],
+      synchronize: process.env.TYPEORM_SYNCHRONIZE === "true" || false
+    })
   ],
   controllers: [AppController],
   providers: [AppService]
